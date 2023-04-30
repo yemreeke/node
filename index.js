1f12b7f11bddec7da829387ac979c07e91de6432
Emre Eke <emre.eke@bulutmd.com>
2023-04-30 13:53:42 +0300

    child_process added.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const { spawn } = require('child_process')
 
 const app = express();
+
+app.post('/pull', (req, res) => {
+    const gitPull = spawn('git', ['pull'])
+
+    gitPull.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+    })
+
+    gitPull.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`)
+    })
+
+    gitPull.on('close', (code) => {
+        console.log(`child process exited with code ${code}`)
+        res.send(`Git pull finished with code ${code}`)
+    })
+})
 
 app.get('/', (req, res) => {
     res.json({ message: 'Selam Yunus Emre' });
