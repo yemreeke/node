c6671638731f38eafffaa612da6a986a342b6fb9
Emre Eke <emre.eke@bulutmd.com>
2023-04-30 13:35:23 +0300

    gitignore added.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
     console.log('Server listening on port 3000');
 });
