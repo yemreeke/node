6c4d630a75988969cbdda31e092ab7ae6adfbfde
Yunus Emre Eke <47570969+yemreeke@users.noreply.github.com>
2023-04-30 13:52:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Selam Emre' });
+    res.json({ message: 'Selam Yunus Emre' });
 });
 
 app.listen(3000, "0.0.0.0", () => {
