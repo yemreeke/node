c2929ae93b321c4981cf17638a32a4befd405e72
Emre Eke <emre.eke@bulutmd.com>
2023-04-30 13:40:47 +0300

    updated.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
+    res.json({ message: 'Selam Emre' });
 });
 
 app.listen(3000, "0.0.0.0", () => {
